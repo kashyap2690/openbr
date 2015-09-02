43add8a74c77d40c7d6ae7f1abfb4560e351eb38
Jordan Cheney <jordan.cheney@gmail.com>
2015-09-02 19:08:55 -0400

    Updates to cmake

--- openbr/plugins/cmake/jointcascade.cmake
+++ openbr/plugins/cmake/jointcascade.cmake
@@ -1,8 +1,3 @@
-set(BR_WITH_JOINTCASCADE OFF CACHE BOOL "Build JointCascade Face Detector")
-
-if (${BR_WITH_JOINTCASCADE})
-    find_package(JointCascade REQUIRED)
-    set(BR_THIRDPARTY_LIBS ${BR_THIRDPARTY_LIBS} ${JointCascade_LIB})
-else()
+if (NOT ${BR_WITH_DETECTION})
     set(BR_EXCLUDED_PLUGINS ${BR_EXCLUDED_PLUGINS} plugins/classification/jointcascade.cpp)
 endif()
